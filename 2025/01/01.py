96b55416025740c083dc8fcd7fab958f0b1e21bd
Benjamin Nicol <benjamin.nicol17@gmail.com>
2025-12-02 23:39:57 +0100

    feat: solution of day 1 part 2 puzzle

--- 2025/01/01.py
+++ 2025/01/01.py
@@ -1,3 +1,5 @@
+from math import floor
+
 def part_one(rotations):
     result = 0
     point = 50
@@ -14,15 +16,36 @@ def part_one(rotations):
     
 
 def part_two(rotations):
-    
-    pass
+    result = 0
+    point = 50
+    for rotation in rotations:
+        print(rotation)
+        num_zero = 0
+        if rotation[0] == "L":
+            ratio = (point - int(rotation[1:])) / 100
+            if ratio <= 0:
+                num_zero = floor(abs(ratio))
+                if point != 0:
+                    num_zero += 1
+            point = (point - int(rotation[1:])) % 100
+
+            print(ratio)
+        elif rotation[0] == "R":
+            num_zero = floor((point + int(rotation[1:])) / 100)
+            point = (point + int(rotation[1:])) % 100
+
+        print(point)
+        print(num_zero)
+        result += num_zero
+
+    print(result)
 
 if __name__ == "__main__":
-    # Reading lists from input
+    # Reading rotations from input
     rotations = []
     with open("input_01.txt", mode="r") as file:
         for line in file:
             rotations.append(line.strip("\n"))
 
-    part_one(rotations)
-    # part_two(rotations)
+    # part_one(rotations)
+    part_two(rotations)
